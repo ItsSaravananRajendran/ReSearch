45774489ca0270377efef61dfde595be91c4b672
thundebolt <its.saravanan.rajendran@gmail.com>
2020-04-14 23:12:33 +0530

    made as a single file

--- resultRetriver/src/workerTask/main.py
+++ resultRetriver/src/workerTask/main.py
@@ -13,18 +13,20 @@ except ImportError:
 import re
 import json
 from pyserini.search import pysearch
-
-
-
-
+import tensorflow as tf
+import tensorflow_hub as hub
 
 from constructText import reconstructText
 from luceneSearch import getHits
 
 torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-import tensorflow as tf
-import tensorflow_hub as hub
+USE_SUMMARY = True
+FIND_PDFS = False
+
+minDate = '2020/04/02'
+luceneDir = '/home/user/lucene-index-covid-2020-04-03/'
+BIOASQ_DIR = '/home/user/biobert/bioasq-biobert'
 
 
 def embed_useT(module):
@@ -37,6 +39,72 @@ def embed_useT(module):
 
 
 embed_fn = embed_useT('/home/user/sentence_wise_email/module/module_useT')
+
+
+def reconstructText(tokens, start=0, stop=-1):
+    tokens = tokens[start: stop]
+    if '[SEP]' in tokens:
+        sepind = tokens.index('[SEP]')
+        tokens = tokens[sepind+1:]
+    txt = ' '.join(tokens)
+    txt = txt.replace(' ##', '')
+    txt = txt.replace('##', '')
+    txt = txt.strip()
+    txt = " ".join(txt.split())
+    txt = txt.replace(' .', '.')
+    txt = txt.replace('( ', '(')
+    txt = txt.replace(' )', ')')
+    txt = txt.replace(' - ', '-')
+    txt_list = txt.split(' , ')
+    txt = ''
+    nTxtL = len(txt_list)
+    if nTxtL == 1:
+        return txt_list[0]
+    newList =[]
+    for i,t in enumerate(txt_list):
+        if i < nTxtL -1:
+            if t[-1].isdigit() and txt_list[i+1][0].isdigit():
+                newList += [t,',']
+            else:
+                newList += [t, ', ']
+        else:
+            newList += [t]
+    return ''.join(newList)
+
+
+def getHits(query,keywords,searcher):
+    hits = searcher.search(query + '. ' + keywords)
+    n_hits = len(hits)
+    ## collect the relevant data in a hit dictionary
+    hit_dictionary = {}
+    for i in range(0, n_hits):
+        doc_json = json.loads(hits[i].raw)
+        idx = str(hits[i].docid)
+        hit_dictionary[idx] = doc_json
+        hit_dictionary[idx]['title'] = hits[i].lucene_document.get("title")
+        hit_dictionary[idx]['authors'] = hits[i].lucene_document.get("authors")
+        hit_dictionary[idx]['doi'] = hits[i].lucene_document.get("doi")
+
+    ## scrub the abstracts in prep for BERT-SQuAD
+    for idx,v in hit_dictionary.items():
+        abs_dirty = v['abstract']
+        # looks like the abstract value can be an empty list
+        v['abstract_paragraphs'] = []
+        v['abstract_full'] = ''
+        if abs_dirty:
+            # looks like if it is a list, then the only entry is a dictionary wher text is in 'text' key
+            # looks like it is broken up by paragraph if it is in that form.  lets make lists for every paragraph
+            # and a new entry that is full abstract text as both could be valuable for BERT derrived QA
+            if isinstance(abs_dirty, list):
+                for p in abs_dirty:
+                    v['abstract_paragraphs'].append(p['text'])
+                    v['abstract_full'] += p['text'] + ' \n\n'
+
+            # looks like in some cases the abstract can be straight up text so we can actually leave that alone
+            if isinstance(abs_dirty, str):
+                v['abstract_paragraphs'].append(abs_dirty)
+                v['abstract_full'] += abs_dirty + ' \n\n'
+
 
 class searchModel:
     def __init__(self):
