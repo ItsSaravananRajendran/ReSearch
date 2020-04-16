7462e9df3915b5c00916ecc3ad39365fe4b37a14
sangameswaran <blackdrafteaglekrish@gmail.com>
2020-04-16 22:54:32 +0530

    [ContentHandler] Changes for extracting keywords from query
    Signed-off-by: sangameswaran <blackdrafteaglekrish@gmail.com>

--- resultRetriver/src/server.py
+++ resultRetriver/src/server.py
@@ -1,21 +1,26 @@
+import sys
 
 from flask import Flask
-
 from workerTask.main import modelInst
-from celery import Celery
 
+sys.path.append('../../es/es_core/')
+from content_handler import ContentHandler
 
 app = Flask(__name__, instance_relative_config=True)
+ch = ContentHandler()
 
-def getResult(data):
-    return modelInst.getResult(data["query"],data["keywords"])
+
+def get_result(data):
+    return modelInst.get_result(data["query"], data["keywords"])
+
 
 @app.route('/query')
 def query():
     data = request.get_json()
-    inp = {"query":data.get('query',''), "keywords":data.get('keywords','')}
-    result = getResult(inp);
-    return result;
-    
+    inp = {"query": data.get('query', ''), "keywords": ch.get_keywords(data.get('query', ''))}
+    result = get_result(inp);
+    return result
+
+
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0')
+    app.run(host='0.0.0.0')
