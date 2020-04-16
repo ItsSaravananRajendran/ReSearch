218e7ebc5ed1027505a0eca2326b79d8cdf9869c
thundebolt <its.saravanan.rajendran@gmail.com>
2020-04-16 16:44:29 +0530

    Removed Celery

--- resultRetriver/src/server.py
+++ resultRetriver/src/server.py
@@ -5,25 +5,17 @@ from workerTask.main import modelInst
 from celery import Celery
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-    app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+app = Flask(__name__, instance_relative_config=True)
 
+def getResult(data):
+    return modelInst.getResult(data["query"],data["keywords"])
 
-    celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-    celery.conf.update(app.config)
-
-    @celery.task
-    def getResult(data):
-        return modelInst.getResult(data["query"],data["keywords"])
-
-    @app.route('/query')
-    def query():
-        data = request.get_json()
-        inp = {"query":data.get('query',''), "keywords":data.get('keywords','')}
-        result = getResult.delay(inp);
-        return result;
-        
-    return app
+@app.route('/query')
+def query():
+    data = request.get_json()
+    inp = {"query":data.get('query',''), "keywords":data.get('keywords','')}
+    result = getResult(inp);
+    return result;
+    
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0')
